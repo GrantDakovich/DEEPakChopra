dc92961122b509f862ff9ba35237fcaa5e624110
Nick Vander Heyden <vandy.nick@gmail.com>
2019-04-01 19:00:45 -0500

    Data read-in
    Reads in already cleaned tweets from a .csv file and stores them in an array.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import numpy as np
 import pytorch
 import pandas as pd
+import csv
+
+ifile = open("cleanedTweets.csv", "r")  # input file containing cleaned Chopra tweets
+reader = csv.reader(ifile)  # object to read csv ifile
+
+tweets = []  # array to store tweets
+
+# read in tweets, save to array for further cleaning
+for row in reader:
+    tweets.append(row)
+
+ifile.close()  # tweets read in, no longer need input file
