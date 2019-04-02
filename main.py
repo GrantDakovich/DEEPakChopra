5b33904fd0671edeccbcbdb02667593f4252fc64
Nick Vander Heyden <vandy.nick@gmail.com>
2019-04-02 13:56:48 -0500

    Implement build method for CharRNN graphs

--- main.py
+++ main.py
@@ -49,10 +49,10 @@ class CharRNN(object):
     # what mode this object is in (training/sampling), and grad_clip
     # is for preventing exploding gradients
     def __init__(self, num_classes, batch_size=64,
-                num_steps=100, lstm_size=128,
-                num_layers=1, learning_rate=0.001,
-                keep_prob=0.5, grad_clip=5,
-                sampling=False):
+                 num_steps=100, lstm_size=128,
+                 num_layers=1, learning_rate=0.001,
+                 keep_prob=0.5, grad_clip=5,
+                 sampling=False):
         # Set variables to values given by parameters
         self.num_classes = num_classes
         self.batch_size = batch_size
@@ -70,3 +70,72 @@ class CharRNN(object):
             self.saver = tf.train.Saver()
             self.init_op = tf.global_variables_initializer()
 
+
+def build(self, sampling):
+    if sampling:
+        batch_size, num_steps = 1, 1
+    else:
+        batch_size = self.batch_size
+        num_steps = self.num_steps
+    tf_x = tf.placeholder(tf.int32,
+                          shape=[batch_size, num_steps],
+                          name='tf_x')
+    tf_y = tf.placeholder(tf.int32,
+                          shape=[batch_size, num_steps],
+                          name='tf_y')
+    tf_keepprob = tf.placeholder(tf.float32,
+                                 name='tf_keepprob')
+
+    # One-hot encoding:
+    x_onehot = tf.one_hot(tf_x, depth=self.num_classes)
+    y_onehot = tf.one_hot(tf_y, depth=self.num_classes)
+
+    # Build the multi-layer RNN cells
+    cells = tf.contrib.rnn.MultiRNNCell(
+        [tf.contrib.rnn.DropoutWrapper(
+            tf.contrib.rnn.BasicLSTMCell(self.lstm_size),
+            output_keep_prob=tf_keepprob)
+        for _ in range(self.num_layers)])
+
+    # Define the initial state
+    self.initial_state = cells.zero_state(
+        batch_size, tf.float32)
+
+    # Run each sequence step through the RNN
+    lstm_outputs, self.final_state = tf.nn.dynamic_rnn(
+        cells, x_onehot,
+        initial_state=self.initial_state)
+    print('<< lstm_outputs >>', lstm_outputs)
+    seq_output_reshaped = tf.reshape(
+        lstm_outputs,
+        shape=[-1, self.lstm_size],
+        name='seq_output_reshaped')
+
+    logits = tf.layers.dense(
+        inputs=seq_output_reshaped,
+        units=self.num_classes,
+        activation=None,
+        name='logits')
+    probas = tf.nn.softmax(
+        logits,
+        name='probabilities')
+
+    y_reshaped = tf.reshape(
+        y_onehot,
+        shape=[-1, self.num_classes],
+        name='y_reshaped')
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits,
+            labels=y_reshaped),
+        name='cost')
+
+    # Gradient clipping to avoid "exploding gradients"
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(
+        tf.gradients(cost, tvars),
+        self.grad_clip)
+    optimizer = tf.train.AdamOptimizer(self.learning_rate)
+    train_op = optimizer.apply_gradients(
+        zip(grads, tvars),
+        name='train_op')
