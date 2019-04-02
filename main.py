18e042ec3743aa8d98314747552be662d13b7288
Nick Vander Heyden <vandy.nick@gmail.com>
2019-04-01 19:53:23 -0500

    Delete empty line in .csv, update data read in
    So I guess the way I was reading in data was completely wrong for character level RNN so I took extra inspiration from the professor's code.

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import torch
 import pandas as pd
 import csv
 
-ifile = open("cleanedTweets.csv", "r")  # input file containing cleaned Chopra tweets
-reader = csv.reader(ifile)  # object to read csv ifile
+# Character mapping to integers
+with open("cleanedTweets.csv", 'r', encoding='utf-8') as f:tweets=f.read()
+tweetChars = []
+tweetChars = set(tweets)
+char2int = {ch:i for i,ch in enumerate(tweetChars)}
+int2char = dict(enumerate(tweetChars))
+text_ints = np.array([char2int[ch] for ch in tweets],dtype=np.int32)
 
-tweets = []  # array to store tweets
-
-# read in tweets, save to array for further cleaning
-for row in reader:
-    tweets.append(row)
-
-ifile.close()  # tweets read in, no longer need input file
+print(text_ints)
