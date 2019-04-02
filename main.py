9311c0badd198383bcca5d41e70342bfd04329c8
Nick Vander Heyden <vandy.nick@gmail.com>
2019-04-02 13:43:30 -0500

    Implement CharRNN constructor
    Implemented CharRNN with comments to clarify certain lines of code

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import pandas as pd
 import csv
+import tensorflow as tf
+import os
 
 ########## BEGIN CharRNN ##########
 # Character mapping to integers
@@ -13,6 +15,7 @@ int2char = dict(enumerate(tweetChars))
 text_ints = np.array([char2int[ch] for ch in tweets],dtype=np.int32)
 
 print(text_ints)
+
 
 # Function for splitting data
 def split_data(sequence, batch_size, num_steps):
@@ -31,9 +34,39 @@ def split_data(sequence, batch_size, num_steps):
     output = np.stack(split_output)
     return inputs, output
 
+
 def create_batch_generator(data_x, data_y, num_steps):
     batch_size, total_length = data_x.shape
     num_batches = int(total_length/num_steps)
     for b in range(num_batches):
         yield (data_x[:, b*num_steps:(b+1)*num_steps],
                data_y[:, b*num_steps:(b+1)*num_steps])
+
+
+# Class for character level recurrent neural network
+class CharRNN(object):
+    # Constructor (note, the sampling parameter is for determining
+    # what mode this object is in (training/sampling), and grad_clip
+    # is for preventing exploding gradients
+    def __init__(self, num_classes, batch_size=64,
+                num_steps=100, lstm_size=128,
+                num_layers=1, learning_rate=0.001,
+                keep_prob=0.5, grad_clip=5,
+                sampling=False):
+        # Set variables to values given by parameters
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        self.lstm_size = lstm_size
+        self.num_layers = num_layers
+        self.learning_rate = learning_rate
+        self.keep_prob = keep_prob
+        self.grad_clip = grad_clip
+        self.g = tf.Graph()
+
+        with self.g.as_default():
+            tf.set_random_seed(123)
+            self.build(sampling=sampling)  # builds x and y graphs of data
+            self.saver = tf.train.Saver()
+            self.init_op = tf.global_variables_initializer()
+
